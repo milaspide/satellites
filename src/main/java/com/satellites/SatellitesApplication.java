02091f6963e4bd8d34b761af6f591dc5643834d2
Trpevski <milo.trpevski@accenture.com>
2025-05-05 18:50:00 +0200

    progress on the algorithm of calculation of max number satellites

--- src/main/java/com/satellites/SatellitesApplication.java
+++ src/main/java/com/satellites/SatellitesApplication.java
@@ -1,10 +1,13 @@
 package com.satellites;
 
+import com.satellites.entity.TimeInterval;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class SatellitesApplication {
 
@@ -14,13 +17,13 @@ public class SatellitesApplication {
             return;
         }
 
-        String percorsoFile = args[0];
+        String filePath = args[0];
 
-        try (BufferedReader br = new BufferedReader(new FileReader(percorsoFile))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String linea;
-            int maxNumberOfSatellites = 0;
-            LocalTime startTimeOfMaxInterval = LocalTime.MAX;
-            LocalTime endTimeOfMaxInterval = LocalTime.MAX;
+            List<TimeInterval> intervals = new ArrayList<>();
+
+            //Leggo il file e inserisco gli intervalli in una lista ordinabile
             while ((linea = br.readLine()) != null) {
                 String startTimeString = linea.substring(0, linea.indexOf(","));
                 String endTimeString = linea.substring(linea.indexOf(",") + 1);
@@ -28,10 +31,92 @@ public class SatellitesApplication {
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
                 LocalTime startTime = LocalTime.parse(startTimeString, formatter);
                 LocalTime endTime = LocalTime.parse(endTimeString, formatter);
-                LocalTime daVerificare = LocalTime.now();
-
-                boolean compreso = !daVerificare.isBefore(startTime) && !daVerificare.isAfter(endTime);
+                // aggiungo l'intervallo alla lista
+                intervals.add(new TimeInterval(startTime, endTime));
             }
+            //Ordino la lista per ora di inizio
+//            intervals.sort(Comparator.comparing(TimeInterval::getStartTime));
+
+            //Dichiaro le variabili globali
+            int maxNumberOfSatellites = 0;
+            LocalTime startTimeOfMaxInterval = null;
+            LocalTime endTimeOfMaxInterval = null;
+
+            // Stampa intervalli ordinati
+            /*for (int i = 1; i < intervals.size(); i++) {
+                TimeInterval interval = intervals.get(i);
+
+                System.out.println(interval.toString());
+
+                LocalTime startTime = interval.getStartTime();
+                LocalTime endTime = interval.getEndTime();
+
+                boolean isIncludedInPrev = false;
+                int numberOfSatellites = 1;
+
+                List<TimeInterval> previousIntervals = intervals.subList(0, i - 1);
+
+                for (int j = i; j >= 0; j--) {
+
+                    if (!startTime.isAfter(previousIntervals.get(j).getEndTime())) {
+                        numberOfSatellites++;
+                        startTimeOfMaxInterval = startTime;
+                        //
+                        endTimeOfMaxInterval = endTime;
+                    } else {
+                        numberOfSatellites--;
+                    }
+                }
+            }*/
+
+            List<Integer> numberOfStartInclusionsList = new ArrayList<>();
+            List<Integer> numberOfEndInclusionsList = new ArrayList<>();
+            for (int i = 0; i < intervals.size(); i++) {
+
+                TimeInterval interval = intervals.get(i);
+
+                List<TimeInterval> otherIntervals = intervals.stream().filter(e ->
+                        !e.equals(interval)).toList();
+
+                int numberOfIntervalsStartIsIncluded = otherIntervals.stream().filter(e ->
+                        !interval.getStartTime().isBefore(e.getStartTime()) && !interval.getStartTime().isAfter(e.getEndTime())).toList().size();
+
+                numberOfStartInclusionsList.add(numberOfIntervalsStartIsIncluded);
+
+                int numberOfIntervalsEndIsIncluded = otherIntervals.stream().filter(e ->
+                        !interval.getEndTime().isBefore(e.getStartTime()) && !interval.getEndTime().isAfter(e.getEndTime())).toList().size();
+
+                numberOfEndInclusionsList.add(numberOfIntervalsEndIsIncluded);
+
+            }
+            Integer maxNumberOfStart = Collections.max(numberOfStartInclusionsList);
+            System.out.println(maxNumberOfStart);
+
+            List<Integer> massimiInizio = numberOfStartInclusionsList.stream().filter(e ->
+                    e.equals(maxNumberOfStart)).toList();
+
+//            System.out.println(numberOfStartInclusionsList.stream().filter(e -> e < maxNumberOfStart).toList().size());
+            System.out.println(massimiInizio.size());
+
+            int indexOfMaxStart = numberOfStartInclusionsList.indexOf(maxNumberOfStart);
+
+            startTimeOfMaxInterval = intervals.get(indexOfMaxStart).getStartTime();
+
+            Integer maxNumberOfEnd = Collections.max(numberOfEndInclusionsList);
+            System.out.println(maxNumberOfEnd);
+
+            List<Integer> massimiFine = numberOfEndInclusionsList.stream().filter(e ->
+                    e.equals(maxNumberOfEnd)).toList();
+
+            System.out.println(massimiFine.size());
+
+            int indexOfMaxEnd = numberOfStartInclusionsList.indexOf(maxNumberOfEnd);
+
+            endTimeOfMaxInterval = intervals.get(indexOfMaxEnd).getEndTime();
+
+            if (Objects.equals(maxNumberOfStart, maxNumberOfEnd))
+                maxNumberOfSatellites = maxNumberOfStart;
+
             System.out.println(startTimeOfMaxInterval + "-" + endTimeOfMaxInterval + ";" + maxNumberOfSatellites);
         } catch (IOException e) {
             System.err.println("Errore nella lettura del file: " + e.getMessage());
---
+++ src/main/java/com/satellites/entity/TimeInterval.java
@@ -0,0 +1,37 @@
+package com.satellites.entity;
+
+import java.time.LocalTime;
+
+public class TimeInterval {
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public TimeInterval(LocalTime startTime, LocalTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeInterval{" +
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+}
